6783c0f771e542675c22cac2829ccd9d6ab062d9
Patrick Davison <davisonpatrickj@gmail.com>
2024-07-20 12:21:06 -0500

    changed giphy key name to react_app_giphy...

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const favoriteRouter = require('./routes/favorite.router');
 const categoryRouter = require('./routes/category.router');
 
-const GIPHY_API_KEY = process.env.GIPHY_API_KEY; 
+const REACT_APP_GIPHY_KEY = process.env.REACT_APP_GIPHY_KEY; 
 
 const PORT = process.env.PORT || 5001;
 
@@ -15,8 +15,8 @@ app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 app.get('/api_key', (req, res) => {
-  res.send(GIPHY_API_KEY);
-  res.json({GIPHY_API_KEY: GIPHY_API_KEY})
+  res.send(REACT_APP_GIPHY_KEY);
+  // res.json({REACT_APP_GIPHY_KEY: REACT_APP_GIPHY_KEY})
 });
 app.use('/api/favorites', favoriteRouter);
 app.use('/api/categories', categoryRouter);
