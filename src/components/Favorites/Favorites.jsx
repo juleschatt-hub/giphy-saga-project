e80be1c53bbc9b91c68f466ae1f92abd77f33e23
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-07-23 11:59:53 -0500

    reducer and saga to store favorites working. /api/vavorites now fetching favorte gifs from DB

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -2,51 +2,38 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 
 
-function FavoriteView() {
-    // const favorites = [{
-    //     url: "https://upload.wikimedia.org/wikipedia/commons/7/7d/Wildlife_at_Maasai_Mara_%28Lion%29.jpg",
-    //     title: "sample",
-    //     category: "wild"
-    // }, {
-    //     url: "https://upload.wikimedia.org/wikipedia/commons/7/7d/Wildlife_at_Maasai_Mara_%28Lion%29.jpg",
-    //     title: "sample 2",
-    //     category: "wild"
-    // }, {
-    //     url: "https://upload.wikimedia.org/wikipedia/commons/7/7d/Wildlife_at_Maasai_Mara_%28Lion%29.jpg",
-    //     title: "sample 3",
-    //     category: "wild"
-    // }]
-
+function Favorites() {
     const dispatch = useDispatch();
     const categoriesList = useSelector(store => store.categoriesList);
     useEffect(() => {
-        dispatch({type: 'FETCH_CATEGORIES'})
+        dispatch({type: 'FETCH_CATEGORIES'});
 
     }, []);
 
     const favorites = useSelector(store => store.favorites);
 
     useEffect(() => {
-        dispatch({type: 'GET_FAVORITES'})
-    }, [])
+        dispatch({type: 'FETCH_FAVORITES'});
+    }, []);
 
     return (
-    
-    <div className="favorites">
-        {favorites.map((favorite) => (
-            <div className="favorite">
-                <img src={favorite.gif_url} alt="" width={200} height={200} />
-                <select name="categories" id="categories">
-                    <option value="">Select Category</option>
-                    {categoriesList.map((category) => (
-                        <option value="{category.name}">{category.name}</option>
-                    ))}
-                </select>
-            </div>
-        ))}
-    </div>
+    <>
+      <div className="favorites">
+         {favorites.map((favorite) => (
+             <div className="favorite" > 
+                 <img key={favorites.id} src={favorite.gif_url} alt="" width={200} height={200} />
+                  <select name="categories" id="categories">
+                     <option value="">Select Category</option>
+                     {categoriesList.map((category) => (
+                         <option value="{category.name}" key={category.id}>{category.name}</option>
+                     ))}
+                 </select>
+             </div>
+         ))}
+     </div> 
+    </>
 )
 
 }
 
-export default FavoriteView
+export default Favorites
--- src/redux/store.js
+++ src/redux/store.js
@@ -15,13 +15,13 @@ const categoriesList = (state = [], action) => {
     }
 }
 
-let initialState = []
-const favorites = (state = initialState, action) => {
+
+
+const favorites = (state = [], action) => {
     switch(action.type){
-        // case 'ADD_FAVORITE':
-        //     return [...state, action.payload]
         case 'GET_FAVORITES':
             return action.payload
+                      
         default:
             return state
     }
@@ -42,28 +42,30 @@ function* fetchFavorites() {
     try {
         const favoritesResponse = yield axios.get('/api/favorites');
         yield put({type: 'GET_FAVORITES', payload: favoritesResponse.data});
+        //console.log('favorites response:', favoritesResponse)
     }
     catch(error) {
         console.log('error fetching favorites', error)
     }
 }
-function* addFavorite(action) {
-    try {
-        const favoritesResponse = axios({method: 'POST', 
-            url: '/api/favorites', 
-            data: {name: action.payload.name}});
-            yield put({type: 'ADD_FAVORITE'});
-    }
-    catch(error) {
-        console.log('error adding to favorites', error);
-    }
-}
+// function* addFavorite(action) {
+//     try {
+//         const favoritesResponse = axios({method: 'POST', 
+//             url: '/api/favorites', 
+//             data: {name: action.payload.name}});
+//             yield put({type: 'ADD_FAVORITE'});
+//     }
+//     catch(error) {
+//         console.log('error adding to favorites', error);
+//     }
+// }
 
 //SAGAS
 function* rootSaga() {
-    yield takeEvery('FETCH_CATEGORIES', fetchCategories);
-    yield takeEvery('ADD_FAVORITE', addFavorite);
     yield takeEvery('FETCH_FAVORITES', fetchFavorites);
+    yield takeEvery('FETCH_CATEGORIES', fetchCategories);
+    // yield takeEvery('ADD_FAVORITE', addFavorite);
+    
     
 }
 
@@ -72,8 +74,9 @@ const sagaMiddleware = createSagaMiddleware();
 //REDUX STORE
 const store = createStore(
     combineReducers(
-        {categoriesList},
-        {favorites},
+        {categoriesList,
+        favorites
+        }
 
     ),
     applyMiddleware(sagaMiddleware, logger)
